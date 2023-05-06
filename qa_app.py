7ac1db4beef30e686fa2133410c8f870d5984ee2
ckloh720 <132682061+ckloh720@users.noreply.github.com>
2023-05-06 14:40:34 +0800

    Update qa_app.py

--- qa_app.py
+++ qa_app.py
@@ -253,8 +253,8 @@ def main():
 
         chat_openai = ChatOpenAI(
             streaming=True, callback_manager=callback_manager, verbose=True, temperature=0)
-        #qa = RetrievalQA.from_chain_type(llm=chat_openai, retriever=retriever, chain_type="stuff", verbose=True)
-        qa = RetrievalQA.from_chain_type(llm=chat_openai, retriever=self.vectorstore.as_retriever())
+        qa = RetrievalQA.from_chain_type(llm=chat_openai, retriever=retriever, verbose=True)
+      )
         
         # Check if there are no generated question-answer pairs in the session state
         if 'eval_set' not in st.session_state:
